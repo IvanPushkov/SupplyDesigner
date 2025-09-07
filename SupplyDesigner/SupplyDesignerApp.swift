e8f9f4f0949ca1aa91cee811d3b9ee5e1f4fadeb
IvanPushkov <pushkov.ol@yandex.ru>
2025-09-07 22:48:22 +0300

    Initial Commit

---
+++ SupplyDesigner/SupplyDesignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupplyDesignerApp.swift
+//  SupplyDesigner
+//
+//  Created by Ivan on 07.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SupplyDesignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
